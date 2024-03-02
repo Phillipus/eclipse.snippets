b1365c854ee24637b3d4536588bf66a3ec0402dd
Phillipus <p.beauvoir@dadabeatnik.com>
2024-03-02 17:15:54 +0000

    Add Item height snippets

---
+++ src/swt/MacListItemHeight.java
@@ -0,0 +1,61 @@
+package swt;
+import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * List item heights should not shrink when setting font
+ * See https://github.com/eclipse-platform/eclipse.platform.swt/issues/677
+ */
+public class MacListItemHeight {
+
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("List Row Height");
+        shell.setLayout(new GridLayout());
+        
+        List list = new List(shell, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+        GridDataFactory.create(GridData.FILL_HORIZONTAL).applyTo(list);
+        
+        String[] items = new String[10];
+        for(int i = 0; i < 10; i++) {
+            items[i] = "Item " + i;
+        }
+        list.setItems(items);
+        
+        Label label1 = new Label(shell, SWT.NONE);
+        Label label2 = new Label(shell, SWT.NONE);
+        updateLabels(list, label1, label2);
+        
+        Button button = new Button(shell, SWT.PUSH);
+        button.setText("Set font");
+        button.addSelectionListener(widgetSelectedAdapter(e -> {
+            // Set list font to the existing table font
+            list.setFont(list.getFont());
+            updateLabels(list, label1, label2);
+        }));
+        
+        shell.setSize(500, 450);
+        shell.open();
+        
+        while(!shell.isDisposed()) {
+            if(!display.readAndDispatch()) display.sleep();
+        }
+        
+        display.dispose();
+    }
+
+    private static void updateLabels(List list, Label label1, Label label2) {
+        label1.setText("List font: " + list.getFont().getFontData()[0]);
+        label2.setText("List item height: " + list.getItemHeight());
+    }
+}
---
+++ src/swt/MacTableItemHeight.java
@@ -0,0 +1,64 @@
+package swt;
+
+import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+
+/**
+ * 
+ * Table item heights should not shrink when setting font
+ * See https://github.com/eclipse-platform/eclipse.platform.swt/issues/677
+ * 
+ */
+public class MacTableItemHeight {
+
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("Table Font");
+        shell.setLayout(new GridLayout());
+        
+        Table table = new Table(shell, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+        GridDataFactory.create(GridData.FILL_HORIZONTAL).applyTo(table);
+        
+        for(int i = 0; i < 11; i++) {
+            TableItem item = new TableItem(table, 0);
+            item.setText("Item " + i);
+        }
+        
+        Label label1 = new Label(shell, SWT.NONE);
+        Label label2 = new Label(shell, SWT.NONE);
+        updateLabels(table, label1, label2);
+        
+        Button button = new Button(shell, SWT.PUSH);
+        button.setText("Set font");
+        button.addSelectionListener(widgetSelectedAdapter(e -> {
+            // Set table font to the existing table font
+            table.setFont(table.getFont());
+            updateLabels(table, label1, label2);
+        }));
+        
+        shell.setSize(500, 450);
+        shell.open();
+        
+        while(!shell.isDisposed()) {
+            if(!display.readAndDispatch()) display.sleep();
+        }
+        
+        display.dispose();
+    }
+
+    private static void updateLabels(Table table, Label label1, Label label2) {
+        label1.setText("Table font: " + table.getFont().getFontData()[0]);
+        label2.setText("Table item height: " + table.getItemHeight());
+    }
+}
