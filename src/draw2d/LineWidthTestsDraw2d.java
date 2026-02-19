42d54183e1bd9b6c944041808e0eb3c30063a376
Phillipus <p.beauvoir@dadabeatnik.com>
2026-02-19 18:18:30 +0000

    Refactor LineWidthTestsDraw2d

--- src/draw2d/LineWidthTestsDraw2d.java
+++ src/draw2d/LineWidthTestsDraw2d.java
@@ -16,70 +16,85 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Spinner;
 
-import utils.Utils;
-
 /**
  * LineWidth adjustments
  */
+@SuppressWarnings("restriction")
 public class LineWidthTestsDraw2d {
     
-    static int layeredPaneScale = 1;
-    static int lineWidth = 1;
-    static ScalableFreeformLayeredPane layeredPane;
+    public static void main(String[] args) {
+        new LineWidthTestsDraw2d();
+    }
     
-    static class TestFigure extends Figure {
+    private static class TestFigure extends Figure {
+        int lineWidth = 1;
+        
         TestFigure() {
-            setOpaque(true);
-            setBackgroundColor(new Color(181, 255, 255));
+            setBounds(new Rectangle(10, 10, 120, 55));
+        }
+        
+        void setLineWidth(int lineWidth) {
+            this.lineWidth = lineWidth;
+            repaint();
         }
         
         @Override
         protected void paintFigure(Graphics graphics) {
+            graphics.setBackgroundColor(new Color(181, 255, 255));
             graphics.fillRectangle(getBounds());
-            graphics.setLineWidth(lineWidth);
+            
             Rectangle rect = getBounds().getCopy();
-            setLineWidthAdjustment(graphics, lineWidth, rect);
+            setLineWidth(graphics, lineWidth, rect);
             graphics.drawRectangle(rect);
         }
         
-        @SuppressWarnings("restriction")
-        void setLineWidthAdjustment(Graphics graphics, int lineWidth, Rectangle figureBounds) {
+        void setLineWidth(Graphics graphics, int lineWidth, Rectangle figureBounds) {
+            graphics.setLineWidth(lineWidth);
+            
             boolean useLineOffset = "win32".equals(SWT.getPlatform()) && DPIUtil.getDeviceZoom() > 100;
             
             double scale = graphics.getAbsoluteScale();
             
-            // Not needed here
-            //if("win32".equals(SWT.getPlatform())) {
-                //scale = scale / (DPIUtil.getDeviceZoom() / 100);
-            //}
+            // If scale is below 100% bottom and right hand rectangle line might not be drawn
+            if(scale < 1.0) {
+                figureBounds.resize(-1, -1);
+            }
             
-            figureBounds.resize(-lineWidth, -lineWidth);
-            
+            // If line width is 1 and scale is 100% and don't use line offset then don't apply an offset
+            // useLineOffset is false if Mac/Linux or user set preference off on Windows
+            // Typically we want it on for Windows where display scaling > 100%
             if(lineWidth == 1 && scale == 1.0 && !useLineOffset) {
+                figureBounds.resize(-1, -1);
                 return;
             }
             
+            // If linewidth is even shrink by half of lineWidth
+            if((lineWidth & 1) == 0) { // 
+                figureBounds.shrink(lineWidth / 2, lineWidth / 2);
+                return;
+            }
+            
+            // If linewidth is odd...
+            figureBounds.resize(-lineWidth, -lineWidth);
+            
+            // x,y offset is half of line width
             float offset = (float)lineWidth / 2;
             
+            // If this is a non hi-res device and scale == 100% round up to integer to stop anti-aliasing
             if(DPIUtil.getDeviceZoom() == 100 && scale == 1.0) {
                 offset = (float)Math.ceil(offset);
             }
             
             graphics.translate(offset, offset);
         }
-        
-        void setLineWidthAdjustment2(Graphics graphics, int lineWidth, Rectangle figureBounds) {
-            figureBounds.shrink(lineWidth, lineWidth);
-        }
-    }    
-
-    public static void main(String[] args) {
-        // Ensure this is set
-        System.setProperty(Utils.SWT_AUTOSCALE_UPDATE_ON_RUNTIME, "true");
-        
-        // Drawd2d autoscale
-        System.setProperty(Utils.DRAW2D_ENABLE_AUTOSCALE, "true");
-        
+    }
+    
+    private double scale = 1;
+    private ScalableFreeformLayeredPane layeredPane;
+    private TestFigure figure;
+    private Spinner scaleSpinner;
+    
+    LineWidthTestsDraw2d() {
         Display display = new Display();
         Shell shell = new Shell(display);
         shell.setLayout(new GridLayout(2, false));
@@ -88,13 +103,16 @@ public class LineWidthTestsDraw2d {
         Label label = new Label(shell, 0);
         label.setText("Scale:");
         
-        Spinner scaleSpinner = new Spinner(shell, 0);
+        scaleSpinner = new Spinner(shell, SWT.READ_ONLY);
         GridDataFactory.swtDefaults().applyTo(scaleSpinner);
-        scaleSpinner.setMinimum(1);
-        scaleSpinner.setMaximum(8);
+        scaleSpinner.setMinimum(25);
+        scaleSpinner.setMaximum(800);
+        scaleSpinner.setIncrement(25);
+        scaleSpinner.setSelection(100);
+        scaleSpinner.setDigits(2);
         scaleSpinner.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> {
-            layeredPaneScale = scaleSpinner.getSelection();
-            layeredPane.setScale(layeredPaneScale);
+            scale = scaleSpinner.getSelection() / 100.0f;
+            layeredPane.setScale(scale);
         }));
         
         label = new Label(shell, 0);
@@ -105,30 +123,19 @@ public class LineWidthTestsDraw2d {
         linewidthSpinner.setMinimum(1);
         linewidthSpinner.setMaximum(4);
         linewidthSpinner.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> {
-            lineWidth = linewidthSpinner.getSelection();
-            layeredPane.repaint();
+            figure.setLineWidth(linewidthSpinner.getSelection());
         }));
-        
-        FigureCanvas fc = new FigureCanvas(shell);
-        GridDataFactory.defaultsFor(fc).span(2, 1).applyTo(fc);
-        
-        fc.addListener(SWT.MouseVerticalWheel, e -> {
-            if((e.stateMask & SWT.MOD1) != 0) {
-                layeredPaneScale += e.count < 0 ? -1 : 1;
-                layeredPaneScale = Math.clamp(layeredPaneScale, 1, 8);
-                layeredPane.setScale(layeredPaneScale);
-                scaleSpinner.setSelection(layeredPaneScale);
-            }
-        });
         
         layeredPane = new ScalableFreeformLayeredPane();
         layeredPane.setOpaque(true);
         layeredPane.setBackgroundColor(new Color(255, 255, 255));
+
+        figure = new TestFigure();
+        layeredPane.add(figure);
+
+        FigureCanvas fc = createFigureCanvas(shell);
         fc.setContents(layeredPane);
-        
-        TestFigure textFigure = new TestFigure();
-        textFigure.setBounds(new Rectangle(5, 5, 120, 55));
-        layeredPane.add(textFigure);
+        fc.setFocus();
         
         shell.open();
         
@@ -137,5 +144,33 @@ public class LineWidthTestsDraw2d {
                 display.sleep();
             }
         }
+    }
+    
+    private FigureCanvas createFigureCanvas(Shell shell) {
+        FigureCanvas fc = new FigureCanvas(shell);
+        GridDataFactory.defaultsFor(fc).span(2, 1).applyTo(fc);
+        
+        fc.addListener(SWT.MouseVerticalWheel, e -> {
+            if((e.stateMask & SWT.MOD1) != 0) {
+                double inc = e.count < 0 ? -0.25 : 0.25;
+                setScale(inc);
+            }
+        });
+        
+        fc.addListener(SWT.KeyDown, e -> {
+            double inc = e.character == '-' ? -0.25 : e.character == '+' || e.character == '=' ?  0.25 : 0;
+            setScale(inc);
+        });
+        
+        return fc;
+    }
+    
+    private void setScale(double inc) {
+        scale = Math.clamp(scale + inc , 0.25, 8);
+        layeredPane.setScale(scale);
+        scaleSpinner.setSelection((int)(scale * 100));
+        
+        //int pos = (int)Math.ceil(10 / scale);
+        //figure.setLocation(new Point(pos, pos));
     }
 }
